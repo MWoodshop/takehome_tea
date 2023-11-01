66a03a20161186a5a78e52cd4f0b4b29b77a11b0
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-11-01 11:47:01 -0400

    Chore: ignores lines for simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,9 +26,11 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
+  # :nocov:
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
+  # :nocov:
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
