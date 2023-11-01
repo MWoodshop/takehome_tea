2d2254f63eeccf09768f4b38e97c4507abfee4a6
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-11-01 11:06:39 -0400

    Test: Updates models with validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,9 @@ class Subscription < ApplicationRecord
   has_many :teas, through: :subscriptions_teas
   has_many :customers_subscriptions
   has_many :customers, through: :customers_subscriptions
+
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :status, presence: true
+  validates :frequency, presence: true
 end
--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,9 @@
 class Tea < ApplicationRecord
   has_many :subscriptions_teas
   has_many :subscriptions, through: :subscriptions_teas
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :temperature, presence: true
+  validates :brewtime, presence: true
 end
